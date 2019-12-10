c277ecf3cdc6270f1e2de7bf7613c4a7590a8c1a
Rafael Thomas Salgado <thomassalgado@gmail.com>
2019-12-10 16:38:32 +0100

    Update initdb-postgis.sh

--- initdb-postgis.sh
+++ initdb-postgis.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
@@ -6,19 +6,12 @@ set -e
 export PGUSER="$POSTGRES_USER"
 
 # Create the 'template_postgis' template db
-"${psql[@]}" <<- 'EOSQL'
-CREATE DATABASE template_postgis;
-UPDATE pg_database SET datistemplate = TRUE WHERE datname = 'template_postgis';
-EOSQL
+echo "Loading PostGIS extensions into $POSTGRES_DB"
 
-# Load PostGIS into both template_database and $POSTGRES_DB
-for DB in template_postgis "$POSTGRES_DB"; do
-	echo "Loading PostGIS extensions into $DB"
-	"${psql[@]}" --dbname="$DB" <<-'EOSQL'
-		CREATE EXTENSION IF NOT EXISTS postgis;
-		CREATE EXTENSION IF NOT EXISTS postgis_topology;
-		CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;
-		CREATE EXTENSION IF NOT EXISTS postgis_tiger_geocoder;
-		CREATE EXTENSION IF NOT EXISTS btree_gist;
+psql --dbname="$POSTGRES_DB" <<-'EOSQL'
+	CREATE EXTENSION IF NOT EXISTS postgis;
+	CREATE EXTENSION IF NOT EXISTS postgis_topology;
+	CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;
+	CREATE EXTENSION IF NOT EXISTS postgis_tiger_geocoder;
+	CREATE EXTENSION IF NOT EXISTS btree_gist;
 EOSQL
-done
